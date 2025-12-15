0f78cc1214bc31a85f040250c3311ab693b301e3
AshmitSrivastava <ashmitsrivastava1607@gmail.com>
2025-12-16 00:58:39 +0530

    a

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -84,7 +84,7 @@ const Hero = () => {
           playsInline
           className="absolute inset-0 w-full h-full object-cover"
         >
-          <source src="/videos/HERO_BACKGROUND1.mp4" type="video/mp4" />
+          <source src="/public/videos/HERO_BACKGROUND1.mp4" type="video/mp4" />
         </video>
         <div className="absolute inset-0 bg-black/25" />
       </div>
