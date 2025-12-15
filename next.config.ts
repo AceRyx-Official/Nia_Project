63eb239a7ee66a4bcd1c18f03634cc442ae8eaaf
AshmitSrivastava <ashmitsrivastava1607@gmail.com>
2025-12-16 01:05:05 +0530

    aa

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // Enables static exports
+  reactStrictMode: true,
+  // If deploying to a subpath like 'yourusername.github.io/yourreponame'
+  basePath: "/Nia_Project", 
+  assetPrefix: "/Nia_Project/", // Required for assets like images
   images: {
-    unoptimized: true,
+    unoptimized: true, // Disable Next.js image optimization as it needs a server
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
